256372b2fa92d52296b5d00bd21a5c68741e88d7
SamadhanManjare <122026715+SamadhanManjare@users.noreply.github.com>
2025-07-19 23:03:18 +0530

    1

--- controllers/Course.js
+++ controllers/Course.js
@@ -8,6 +8,31 @@ exports.createCourse = async (req,res) => {
 
     try{
 
+        const {courseName, courseDescription, Whatyouwilllearn, price, tag} = req.body;
+
+        //get thumbnail
+        const thumbnail = req.files.thumbnailImage;
+
+        //validation 
+        if(!courseName || !courseDescription || !Whatyouwilllearn || !price || !tag){
+            return res.status(400).json({
+                success : false,
+                message : "All fields are required",
+            })
+        }
+
+        //check for instructor
+        const userId = req.User.id;
+        const instructorDetails = await User.findById(userId);
+        console.log("Instructor Details :", instructorDetails);
+
+
+        if(!instructorDetails){
+            return res.status(404).json({
+                success : false,
+                message : "Instructor Details are not found "
+            })
+        }
     }
     catch(error){
         return res.status(500).json({
