47711e393344ac8307fb012573d9f34a2cf8d0d9
SamadhanManjare <122026715+SamadhanManjare@users.noreply.github.com>
2025-07-20 16:26:24 +0530

    Course Updated

--- controllers/Course.js
+++ controllers/Course.js
@@ -33,12 +33,89 @@ exports.createCourse = async (req,res) => {
                 message : "Instructor Details are not found "
             })
         }
+
+        //Check given tag is valid or not
+        const tagDetails = await Tags.findById(tag);
+        if(!tagDetails){
+            return res.status(404).json({
+                success : false,
+                message : "Tag details not found"
+            })
+        }
+
+        //Upload image to cloudinary
+        const thumbnailImage = await imageUploadtoCloudinary(thumbnail, process.env.FOLDER_NAME);
+
+         //create an entry for create course
+    const newCourse = await Course.create({
+        courseName,
+        courseDescription,
+        instructor,
+        whatyouwilllearn,
+        tags : tagDetails._id,
+        price,
+        thumbnail : thumbnailImage.secure_url,
+    })
+
+    //add new course to the user schema of instructor
+    await User.findByIdAndUpdate(
+        {
+            _id : instructorDetails._id
+        },
+        {
+            $push :{
+                courses : newCourse._id,
+            }
+        },
+        {new : true},
+    )
+
+    //Update the tag ka schema
+    // todo : 
+
+    //return response
+    return res.status(200).json (
+        {
+            success : true,
+            message : "Course created successfully",
+            data : newCourse,
+        }
+    )
     }
     catch(error){
+        console.log(error);
         return res.status(500).json({
+            
+
             success : false,
-            message : error.message,
+            message : "Failed to create a course",
+            error : error.message,
         })
     }
     
+   
+}
+
+//get all courses handler function
+
+exports.showAllCourses = async (req,res) => {
+    try{
+         const allCourses = await Course.find({}, {
+            courseName : true,
+           price : true,
+           thumbnail : true,
+           instructor : true,
+           ratingsandreviews : true,
+           studentsEnrolled : true,
+
+         }).populate("instructor").exec();
+    }
+    catch(error){
+         console.log(error);
+         return res.status(500).json({
+            success : false,
+            message : "cannot fetch course data",
+            error : error.message,
+         })
+    }
 }
